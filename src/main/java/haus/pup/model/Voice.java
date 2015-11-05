4e4c47b90963a86814b7793e926092b34f3529b2
Wolfy Meier <wolfy@wlfy.it>
2015-11-04 20:24:42 -0800

    created voice model

--- src/main/java/haus/pup/model/Voice.java
+++ src/main/java/haus/pup/model/Voice.java
@@ -1,4 +1,53 @@
 package haus.pup.model;
 
+import haus.pup.tts.Gender;
+import haus.pup.tts.Lang;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Voice {
+
+  private String name;
+  private Lang lang;
+  private Gender gender;
+
+  public Gender getGender() {
+    return gender;
+  }
+
+  public void setGender(Gender gender) {
+    this.gender = gender;
+  }
+
+  public void setGender(String gender) {
+    gender = gender.toUpperCase(); //Convert to uppercase to match ENUM
+
+    this.gender = Gender.valueOf(gender);
+  }
+
+  public Lang getLang() {
+    return lang;
+  }
+
+  public void setLang(Lang lang) {
+    this.lang = lang;
+  }
+
+  public void setLang(String lang) {
+    lang = lang.replace('-','_'); //Convert dashes to underscores to match ENUM
+    lang = lang.toUpperCase(); //Convert to uppercase to match ENUM
+
+    this.lang = Lang.valueOf(lang);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
 }
